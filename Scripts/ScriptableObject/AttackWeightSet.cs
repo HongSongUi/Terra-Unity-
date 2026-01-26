0cf57e1a7674f955e133149473c556bb6d99cc8c
thddml <thddml40@gmail.com>
2026-01-26 20:44:00 +0900

    update

--- Scripts/ScriptableObject/AttackWeightSet.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
-public class AttackWeightSet : ScriptableObject
-{
-    public List<int> nearWeights;
-    public List<int> midWeights;
-}
